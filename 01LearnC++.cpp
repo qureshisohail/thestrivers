0d85a165a00cb4cea23635596faeddb74d8e636c
qureshisohail <qsohail@live.com>
2024-05-12 15:52:15 +0530

    Commit

---
+++ 01LearnC++.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char x, y;
+    cout << "Enter x & y"<<endl;
+
+
+    if(cin>>x)
+    {
+        cout << "Printed"<<endl;
+    }
+
+    if(cout<<"Hellow"){
+        cout << "Again Printed";
+    }
+}
---
+++ 02_MultiplicationTabel.cpp
@@ -0,0 +1,19 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n= 10,m=10;
+
+    for(int i=1; i<=n;i++)
+    {
+        for(int j = 1; j<=m;j++)
+        {
+            cout<<"\t"<<i*j;
+        }
+
+            cout<<endl;
+
+    }
+    return 0;
+}
